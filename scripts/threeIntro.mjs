24ab520db07652c283f08a63b3f3e4b09833b0f3
songhyun-lee <58297416+songhyun-lee@users.noreply.github.com>
2020-01-31 18:13:37 +0900

    Update threeIntro.mjs

--- scripts/threeIntro.mjs
+++ scripts/threeIntro.mjs
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/110/three.module.js';
+import * as THREE from 'https://threejs.org/build/three.module.js';
 import {GLTFLoader} from 'https://threejs.org/examples/jsm/loaders/GLTFLoader.js';
 
 
