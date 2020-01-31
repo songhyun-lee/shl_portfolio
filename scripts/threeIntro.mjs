3691be75a42c3c28912d374209c36d7d3a738263
songhyun-lee <58297416+songhyun-lee@users.noreply.github.com>
2020-01-31 17:42:03 +0900

    Create threeIntro.mjs

---
+++ scripts/threeIntro.mjs
@@ -0,0 +1,106 @@
+import * as THREE from '../node_modules/three/build/three.module.js';
+import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+
+function main() {
+
+  const canvas = document.querySelector('#c');
+  const renderer = new THREE.WebGLRenderer({ canvas:canvas, alpha: true });
+  renderer.setClearColor(0x333342, 0);
+  const camera = new THREE.PerspectiveCamera(65, 2, .1, 2000);
+  camera.position.set(0, 0, 900);
+  const scene = new THREE.Scene();
+
+  {
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0xeeeeee, 0.9);
+    const dirLight = new THREE.DirectionalLight(0xE1DEE3, 0.05);
+    const pointLight = new THREE.PointLight(0xFFECAC, .8, 5000);
+    pointLight.position.set(-300, 100, -1000);
+    scene.add(pointLight);
+    const backLight = new THREE.DirectionalLight(0xFFECAC, .9);
+    scene.add(backLight);
+    
+    dirLight.position.set(50, 0, 300);
+
+    scene.add(hemiLight);
+    scene.add(dirLight);
+  }
+
+  let root;
+  let horn = new THREE.Object3D();
+  const gltfLoader = new GLTFLoader();
+  gltfLoader.load('../resources/horn.gltf', (gltf) => {
+    root = gltf.scene;
+    root.position.set(0, -300, 400);
+    
+    horn.add(root);
+    scene.add(horn);
+  })
+
+  let mouse = {
+    x: 0,
+    y: 0
+  }
+
+  canvas.addEventListener('mousemove', handleMousemove, false);
+  function handleMousemove(e){
+    mouse.x = e.clientX - canvas.clientWidth/2;
+    mouse.y = -(e.clientY - canvas.clientHeight/2);
+  }
+  canvas.addEventListener('touchmove', handleTouchmove, false);
+  function handleTouchmove(e){
+    mouse.x = e.touches[0].clientX - canvas.clientWidth/2;
+    mouse.y = -(e.touches[0].clientY - canvas.clientHeight/2);
+  }
+
+  function hornRotate(speed){
+    const hornRY = calc(mouse.x, -400, 400, -Math.PI/80, Math.PI/80);
+    horn.rotation.y += (hornRY - horn.rotation.y) / speed;
+    const hornRX = calc(mouse.y, -400, 400, Math.PI/120, -Math.PI/120);
+    horn.rotation.x += (hornRX - horn.rotation.x) / speed;
+  }
+
+  // function dumpObject(obj, lines = [], isLast = true, prefix = '') {
+  //   const localPrefix = isLast ? '└─' : '├─';
+  //   lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`);
+  //   const newPrefix = prefix + (isLast ? '  ' : '│ ');
+  //   const lastNdx = obj.children.length - 1;
+  //   obj.children.forEach((child, ndx) => {
+  //     const isLast = ndx === lastNdx;
+  //     dumpObject(child, lines, isLast, newPrefix);
+  //   });
+  //   return lines;
+  // }
+
+  function calc(vc, vmin, vmax, dmin, dmax){
+    const vil = Math.max(Math.min(vc, vmax), vmin);
+    const vs = vmax - vmin;
+    const vr = (vil - vmin) / vs;
+    const ds = dmax - dmin;
+    const d = ds*vr + dmin;
+    return d;
+  }
+
+  function resizeRenderer(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+  function loop() {
+    hornRotate(40);
+    if (resizeRenderer(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+    renderer.render(scene, camera);
+    requestAnimationFrame(loop);
+  }
+  loop();
+}
+main();
